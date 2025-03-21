50344f807d4e21cff11066b075caa39144d5909d
myreceiptt <nota@straight-line.org>
2025-03-21 11:25:08 +0700

    free claim

--- src/app/free/page.tsx
+++ src/app/free/page.tsx
@@ -6,47 +6,11 @@
 import React from "react";
 
 // Components libraries
-import SouvenirsList from "@/components/contents/SouvenirsList";
+import SouvenirDetails from "@/components/contents/SouvenirDetails";
 import DynamicLoginPage from "@/components/logins/DynamicLogin";
 
-const pageTitle1 = "Freemium";
-const pageTitle2 = "Virtual Collectibles";
-const tokenIds = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-];
-
-const PageFree: React.FC = () => (
-  <DynamicLoginPage
-    ContentComponent={() => (
-      <SouvenirsList
-        tokenIds={tokenIds}
-        title1={pageTitle1}
-        title2={pageTitle2}
-      />
-    )}
-  />
+const PageToken: React.FC = () => (
+  <DynamicLoginPage ContentComponent={() => <SouvenirDetails />} />
 );
 
-export default PageFree;
+export default PageToken;
