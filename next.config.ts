8c274d3b41eb13cf0c247d3fadc540ee9d694b6b
myreceiptt <nota@straight-line.org>
2025-04-13 09:00:21 +0800

    temporary remove search

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,22 @@
 import type { NextConfig } from "next";
-
+const isTurbopack = process.env.NEXT_RUNTIME === "edge"; // Atau pakai flag custom
 const nextConfig: NextConfig = {
   // ✅ Turbopack is now stable — official config
   turbopack: {
     // Tambahkan custom rules di sini kalau diperlukan nanti
   },
 
-  // ✅ Webpack workaround (masih dibutuhkan oleh WalletConnect, dll.)
-  // ✅ Keeps Webpack fixes (if needed)
-  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
-  // webpack: (config) => {
-  //   config.externals.push("pino-pretty", "lokijs", "encoding");
-  //   return config;
-  // },
+  ...(isTurbopack
+    ? {}
+    : {
+        // ✅ Webpack workaround (masih dibutuhkan oleh WalletConnect, dll.)
+        // ✅ Keeps Webpack fixes (if needed)
+        // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
+        webpack: (config) => {
+          config.externals.push("pino-pretty", "lokijs", "encoding");
+          return config;
+        },
+      }),
 };
 
 export default nextConfig;
